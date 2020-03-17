be73733ec98e4f2521b0ec1f1753110067f8e98e
zhouqinsong <2055058027@qq.com>
2020-03-17 20:47:01 +0800

    test commit

---
+++ testGit/src/com/zqs/git/FirstClass.java
@@ -0,0 +1,22 @@
+package com.zqs.git;
+
+/**
+ * @Author zhouqinsong
+ * @注释
+ * @Date 2020/3/17 20:34
+ * @Version 1.0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+    }
+}
